72e29696bb306421f86166b358b008db71a629b9
Ahmed_Elkholy <ahmed_elkholy@f-eng.tanta.edu.eg>
2024-07-29 00:59:34 +0300

    "regular automatic update"

---
+++ Shell/extract.ps1
@@ -0,0 +1,8 @@
+$folderPath = "D:/Projects_Temp/videos_youtube/Shell/srt_file"
+$pythonCommand = "python C:\Users\ahm_e\Documents\Python_Scripts\src\extract_text_from_srt.py"
+# Get all items in the folder and filter only files
+$files = Get-ChildItem -Path $folderPath | Where-Object { -not $_.PSIsContainer }
+# Loop through each file and execute the Python command
+foreach ($file in $files) {
+	Invoke-Expression -Command "$pythonCommand $($file.FullName) -o $($file.FullName).md"
+}
