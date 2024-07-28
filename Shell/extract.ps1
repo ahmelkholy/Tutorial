1b96c8ffe200ebc47912a20e44371c8fcfe4fcf7
Ahmed_Elkholy <ahmed_elkholy@f-eng.tanta.edu.eg>
2024-07-29 01:09:19 +0300

    "regular automatic update"

--- Shell/extract.ps1
+++ Shell/extract.ps1
@@ -1,8 +1,22 @@
-$folderPath = "D:/Projects_Temp/videos_youtube/Shell/srt_file"
-$pythonCommand = "python C:\Users\ahm_e\Documents\Python_Scripts\src\extract_text_from_srt.py"
-# Get all items in the folder and filter only files
-$files = Get-ChildItem -Path $folderPath | Where-Object { -not $_.PSIsContainer }
-# Loop through each file and execute the Python command
-foreach ($file in $files) {
-	Invoke-Expression -Command "$pythonCommand `"$($file.FullName)`" -o `"$($file.FullName).md`""
+# Set the path to the current folder
+$folderPath = Get-Location
+
+# Get all .srt files in the folder
+$srtFiles = Get-ChildItem -Path $folderPath -Filter *.srt
+
+foreach ($file in $srtFiles) {
+	# Construct the full path to the input .srt file and the output .md file
+	$inputFile = $file.FullName
+	$outputFile = [System.IO.Path]::ChangeExtension($inputFile, ".md")
+
+	# Extract text from the .srt file using the Python script
+	$pythonCommand = "python C:\Users\ahm_e\Documents\Python_Scripts\src\extract_text_from_srt.py `"$inputFile`" -o `"$outputFile`""
+	Invoke-Expression $pythonCommand
+
+	# Check if the extraction was successful
+	if (Test-Path $outputFile) {
+		Write-Host "Extraction successful for file: $inputFile"
+	} else {
+		Write-Host "Extraction failed for file: $inputFile"
+	}
 }
